6c655693b37ae017be0a04301dfa2fa76f0804d7
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-02-29 00:14:44 -0500

    variable file created

---
+++ 01_basics/variables.js
@@ -0,0 +1,23 @@
+// How to declare variable and How can we use it ?
+
+// Variable is used to store any information for example user name, user id Etc.
+
+// Here the acc. Id, acc. Email and acc. Password are memory location
+
+const accountId = 8111 // can't be changable
+
+let accountEmail = "harsh@gmail.com" 
+
+var accountPassword = "12345" // We do not use because of scope problem DO NOT USE
+
+accountCity = "Ahemdabad"
+let accountState;
+
+// accountId = 2 // checking constant 
+accountEmail="harshpatel@google.com"
+
+accountPassword="23"
+accountCity="Delhi"
+
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
