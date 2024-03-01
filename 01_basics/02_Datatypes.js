16391875322e9c09ad63b1cc13dd558e341b48ca
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-02-29 23:30:34 -0500

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// it will treat all Js code as newer version
+
+// alert("Hello") This is not working on browser not in nodejs
+
+let name = "Harsh"
+let age = 22
+let isLoggedIn = false
+
+// number, 
+// String,
+//  Boolean-> true/ flase , 
+//  null -> standalone value, undefined -> declare but value not assigned, 
+//  symbol -> Use in react, to find uniqueness
+
+// object
+
+console.log(typeof "harsh");// o/p string
+
+console.log(typeof null); // o/p object
+
+console.log(typeof undefined); // o/p undefined
