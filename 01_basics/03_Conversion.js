326dc479b8755de8daa78ce79fb3d2d3c26b9af8
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-03-02 17:29:08 -0500

    operators

--- 01_basics/03_Conversion.js
+++ 01_basics/03_Conversion.js
@@ -1,29 +1,50 @@
-// let score = 50
-let score = "50"
+// // -----Datatype Coversion ----//
+
+// // let score = 50
+ let score = "50"
 
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNUmber = Number(score)
 
-console.log(typeof valueInNUmber);
+// console.log(typeof valueInNUmber);
 
-// "50" -> convert string to number
-// "50abc" ->  connot convert o/p - NaN
-//true -> 1, false-> 0
+// // "50" -> convert string to number
+// // "50abc" ->  connot convert o/p - NaN
+// //true -> 1, false-> 0
 
 // let isLoggedIn = 1
 let isLoggedIn = "Harsh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// 1-> true
-// 0-> false
-// ""->false
-// "Harsh"->true
+// // 1-> true
+// // 0-> false
+// // ""->false
+// // "Harsh"->true
 
 let exampleNum = 50
 let stringNumber = String(exampleNum)
-console.log(stringNumber);
+// console.log(stringNumber);
+
+//------OPERATIONS----------//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Harsh"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" +
+// 2);
+// console.log(2 + 2+ "1");
+
+let counter = 100
+counter++
+console.log(counter);
