fe5b6fb832446230dc2309a19a18d44b1c082a32
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-03-05 23:04:59 -0500

    Strings manipulation created

---
+++ 01_basics/05_String.js
@@ -0,0 +1,31 @@
+const name = "Harsh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value"); Outdated
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //USE THIS METHOD
+
+// const gameName = new String('BG-MI')
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('M'));
+// console.log(gameName.toLowerCase());
+
+// const newString = gameName.substring(0,2)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5,4)
+// console.log(anotherString)
+
+const newString1 = "  Harsh  "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://harsh.com/harsh%20patel"
+console.log(url.replace('%20',' '))
+
+const myName = "Harsh Patel"
+console.log(myName.split(' '))
+
+
