a166def4c37c0271ef4b582e5fcc1ed0b29152cb
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-03-07 17:25:50 -0500

    Exersice1

---
+++ Exersice/exersice_product_obj.js
@@ -0,0 +1,20 @@
+// let product = {
+//     name:"Parker Jotter Standard CT Ball Pen(Black)",
+//     rating:5,
+//     price:270,
+//     MRP:285,
+//     Discount:5
+
+
+// }
+
+// console.log(product);
+
+let profile = {
+    userName: "HarshPatel",
+    isFollow:false,
+    postCount:1,
+    followersCount:100,
+    followingCount:100
+}
+console.log(profile);
