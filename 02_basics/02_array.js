610f8836d80b9094d1187e525b1097612916e565
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-03-15 18:24:13 -0400

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,26 @@
+const cricketBat = ["SG","MRF","ADIDAS"]
+const cricketBall = ["SG","DUKE","TENNIS"]
+
+// cricketBat.push(cricketBall)
+// console.log(cricketBat);         
+
+// let newArr = cricketBall.concat(cricketBat)
+// console.log(newArr);
+
+// let newArr1 = [...cricketBat,...cricketBall]
+// console.log(newArr1); // Effective method
+
+let mixedArray = [1,2,3,4,[5,6,7],2,3,[1,2,3,[6,8]]]
+let simpleArray = mixedArray.flat(Infinity)
+console.log(simpleArray)
+
+// console.log(Array.isArray("Harsh")); //flase
+// console.log(Array.isArray([1,2,3])); // true
+// console.log(Array.from("Harsh")); // string to array
+// console.log(Array.from({name : "Harsh"})); // it give output as a empty array becuase we have to specify whether we want to give key or value
+
+let var1 = 500
+let var2 = 50
+let var3 = 5
+
+console.log(Array.of(var1,var2,var3)) // create an array
