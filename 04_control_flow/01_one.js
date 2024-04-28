ed360d5c6bd5fd3c97360e20b07db5d083220919
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-04-28 01:11:32 -0400

    if else added

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,20 @@
+// if 
+// operators = >,<,>=,<=,=(assignment),==(comparison),===(check datatype and compare),!=,!
+// let value = 100
+// if (value>50) {
+//     console.log(`${value} is valid`);
+// }else{
+//     console.log("The value is invalid");
+// }
+
+
+let temprature = -1
+let wind = 40
+
+if (temprature<=3) {
+    let jacket = "winterjacket"
+    console.log(`${jacket} is required`);
+}
+// console.log(jacket); // This will give an error becuse of out of scope
+
+
