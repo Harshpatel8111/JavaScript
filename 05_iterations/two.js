287911dfcb85ab53afe487ec270710fb3b96756c
Harshpatel8111 <harshpatel8112001@gmail.com>
2024-05-02 23:46:00 -0400

    while and dowhile loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// // while loop
+// let i = 0
+// while (i < 5) {
+//     console.log(`value of i is ${i}`);
+//     i= i+1
+// }
+
+// let myArray = ["Dhoni", "Kohli", "Sachin"]
+// let i = 0
+// while(i<myArray.length){
+//     console.log(myArray[i]);
+//     i = i+1
+// }
+
+i = 0
+do {
+    console.log(`value of i is ${i}`);
+    i++
+    
+} while (i<=5);
